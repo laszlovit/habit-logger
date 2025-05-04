4a5068b9c955c5662279d5a0a2991319411d0897
laszlovit <laszlo.vitkai@gmail.com>
2025-05-04 15:06:09 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.Sqlite;
+
+using var connection = new SqliteConnection("Data Source=habit.db");
+connection.Open();
+
+var command = connection.CreateCommand();
+command.CommandText =
+    @"CREATE TABLE IF NOT EXISTS habit (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT,
+    quantity INTEGER
+    )";
+
+command.ExecuteNonQuery();
